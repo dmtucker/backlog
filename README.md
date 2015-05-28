0789e8d2a44ef5d6aca912d5aac695e1097beca3
David Tucker <dmtucker@users.noreply.github.com>
2015-05-27 21:32:47 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+I use this tool to choose what to work on during my "personal projects" hour everyday.
---
+++ backlog.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import argparse
+import json
+import random
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-f",
+        dest="backlog",
+        help="Specify a projects file.",
+        default="backlog.json"
+    )
+    args = parser.parse_args()
+    with open(args.backlog, 'r') as f:
+        backlog = json.load(f)
+    selection = []
+    for entry in backlog:
+        selection += [entry["note"]]*entry['priority']
+    print random.choice(selection)
