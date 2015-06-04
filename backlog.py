8c6351259979dffa5dbf27a870ffea682ec7653a
David Tucker <dmtucker@users.noreply.github.com>
2015-06-03 21:21:48 -0700

    Objectify entries. #5

--- backlog.py
+++ backlog.py
@@ -32,7 +32,7 @@ def configure_logging(path):
     return logger
 
 
-def json_file_to_dict(path):
+def json_to_python(path):
     with open(path, 'r') as f:
         return json.load(f)
 
@@ -42,31 +42,60 @@ class Backlog:
     entries = None
 
     def __init__(self, entries=None):
-        for entry in entries:
-            assert entry["priority"] == int(entry["priority"]), "Priorities must be integers."
         self.entries = entries
 
+    def loaded_from(self, json_file=None):
+        self.entries = []
+        if json_file is not None:
+            for entry in json_to_python(json_file):
+                self.entries.append(
+                    Backlog.Entry(
+                        note=entry["note"],
+                        priority=entry.get("priority", 1),
+                        tags=entry.get("tags", ())
+                    )
+                )
+        return self
+
     def random_entry(self):
-        if self.entries is None or len(self.entries) == 0:
+        if self.entries is None or len(self.entries) < 1:
             return None
         selection = []
-        priority_shift = 1-self.lowest_priority()["priority"]
+        priority_shift = 1-self.lowest_priority_entry().priority
         for entry in self.entries:
-            selection.extend([entry["note"]]*(entry["priority"]+priority_shift))
+            selection.extend([entry.note]*(entry.priority+priority_shift))
         return random.choice(selection)
 
-    def lowest_priority(self):
+    def highest_priority_entry(self):
+        highest = None
+        for entry in self.entries:
+            if highest is None or entry.priority > highest.priority:
+                highest = entry
+        return highest
+
+    def lowest_priority_entry(self):
         lowest = None
         for entry in self.entries:
-            if lowest is None or entry["priority"] < lowest["priority"]:
+            if lowest is None or entry.priority < lowest.priority:
                 lowest = entry
         return lowest
+
+    class Entry:
+
+        note = None
+        priority = 1
+        tags = ()
+
+        def __init__(self, note, priority=1, tags=()):
+            self.note = str(note)
+            self.priority = int(priority)
+            self.tags = tuple(tags)
 
 
 if __name__ == "__main__":
     args = parse_cli()
     logger = configure_logging(args.history)
-    backlog = Backlog(entries=json_file_to_dict(args.backlog))
+    backlog = Backlog().loaded_from(json_file=args.backlog)
     entry = backlog.random_entry()
     logger.info(entry)
     print(entry)
