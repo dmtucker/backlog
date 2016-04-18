ca7afda5edfa3653a60bc0bcd6c3ba365a469553
dtucker <david.michael.tucker@gmail.com>
2016-04-17 22:59:08 -0700

    Change the license to LGPLv2+

--- setup.py
+++ setup.py
@@ -21,7 +21,8 @@ setup(
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: End Users/Desktop',
-        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'License :: OSI Approved :: '
+        'GNU Lesser General Public License v2 or later (LGPLv2+)',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
