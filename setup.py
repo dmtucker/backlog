2cf9a67d578161943ea5c624a88e9114a89d7ccd
dtucker <david.michael.tucker@gmail.com>
2016-04-18 21:44:18 -0700

    Make ordering consistent with keysmith

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# coding: utf-8
 
 """A setuptools based setup module.
 
@@ -7,6 +8,7 @@ https://packaging.python.org/en/latest/distributing.html
 https://github.com/pypa/sampleproject
 """
 
+from __future__ import absolute_import
 from setuptools import setup, find_packages
 import backlog
 
@@ -18,20 +20,21 @@ setup(
     version=backlog.__version__,
     description=backlog.__doc__,
     long_description=README,
-    license='LGPLv2+',
-    url='https://github.com/dmtucker/backlog',
     author='David Tucker',
     author_email='david.michael.tucker@gmail.com',
+    license='LGPLv2+',
+    url='https://github.com/dmtucker/backlog',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    include_package_data=True,
+    entry_points={'console_scripts': ['backlog=backlog.__main__:main']},
+    keywords='notes backlog todo lists',
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: End Users/Desktop',
         'License :: OSI Approved :: '
         'GNU Lesser General Public License v2 or later (LGPLv2+)',
+        'Intended Audience :: End Users/Desktop',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-    ],
-    keywords='notes backlog todo lists',
-    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
-    entry_points={'console_scripts': ['backlog=backlog.__main__:main']},
+        'Development Status :: 4 - Beta',
+        ],
 )
