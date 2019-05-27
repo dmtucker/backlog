27c038ae595fbc4e0b699c63afddcc0cdeb4a383
David Tucker <david@tucker.name>
2019-05-26 19:03:33 -0700

    Make setup.py importable

--- setup.py
+++ setup.py
@@ -34,30 +34,34 @@ def find_version(*file_paths):
     raise RuntimeError("Unable to find version string.")
 
 
-setuptools.setup(
-    name='backlog',
-    version=find_version('src', 'backlog', '__init__.py'),
-    description='A Glorified TODO list',
-    long_description=read('README.rst'),
-    author='David Tucker',
-    author_email='david@tucker.name',
-    license='LGPLv2+',
-    url='https://github.com/dmtucker/backlog',
-    package_dir={'': 'src'},
-    packages=setuptools.find_packages('src'),
-    include_package_data=True,
-    python_requires='~= 3.7',
-    install_requires=[
-        'click ~= 6.7',
-    ],
-    entry_points={'console_scripts': ['backlog = backlog.cli:main']},
-    keywords='notes backlog todo list',
-    classifiers=[
-        'License :: OSI Approved :: '
-        'GNU Lesser General Public License v2 or later (LGPLv2+)',
-        'Intended Audience :: End Users/Desktop',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Development Status :: 5 - Production/Stable',
-    ],
-)
+ENTRY_POINTS = {'console_scripts': ['backlog = backlog.cli:main']}
+
+
+if __name__ == '__main__':
+    setuptools.setup(
+        name='backlog',
+        version=find_version('src', 'backlog', '__init__.py'),
+        description='A Glorified TODO list',
+        long_description=read('README.rst'),
+        author='David Tucker',
+        author_email='david@tucker.name',
+        license='LGPLv2+',
+        url='https://github.com/dmtucker/backlog',
+        package_dir={'': 'src'},
+        packages=setuptools.find_packages('src'),
+        include_package_data=True,
+        python_requires='~= 3.7',
+        install_requires=[
+            'click ~= 6.7',
+        ],
+        entry_points=ENTRY_POINTS,
+        keywords='notes backlog todo list',
+        classifiers=[
+            'License :: OSI Approved :: '
+            'GNU Lesser General Public License v2 or later (LGPLv2+)',
+            'Intended Audience :: End Users/Desktop',
+            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8',
+            'Development Status :: 5 - Production/Stable',
+        ],
+    )
--- test_setup.py
+++ test_setup.py
@@ -5,16 +5,32 @@ import os
 import subprocess
 import sys
 
+import setup
+
 
 def test_setup():
     """Run setup.py check."""
-    command = [sys.executable, 'setup.py', 'check', '--metadata', '--strict']
+    command = [
+        sys.executable,
+        setup.__file__,
+        'check',
+        '--metadata',
+        '--strict',
+    ]
     assert subprocess.run(command).returncode == 0
 
 
 def test_console_scripts():
     """Ensure console scripts were installed correctly."""
-    assert any(
-        os.path.isfile(os.path.join(directory, 'backlog'))
-        for directory in os.environ['PATH'].split(':')
+    assert all(
+        any(
+            os.path.isfile(
+                os.path.join(
+                    directory,
+                    console_script.partition('=')[0].strip(),
+                ),
+            )
+            for directory in os.environ['PATH'].split(':')
+        )
+        for console_script in setup.ENTRY_POINTS['console_scripts']
     )
